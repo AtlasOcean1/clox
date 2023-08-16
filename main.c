62399dfeea5e623b0c0071965acb9f3e093788fd
Atlas Ocean <atlas@xygroup.co>
2023-08-16 11:37:18 -0400

    chapter 26

--- main.c
+++ main.c
@@ -62,14 +62,16 @@ static void runFile(const char* path) {
 int main(int argc, const char* argv[]) {
     initVM();
 
-    if(argc == 1) {
-        repl();
-    } else if (argc == 2) {
-        runFile(argv[1]);
-    } else {
-        fprintf(stderr, "Usage: clox [path]\n");
-        exit(64);
-    }
+    runFile("C:\\Users\\Fohr\\hpw\\clox\\test.clox");
+
+    //if(argc == 1) {
+    //    repl();
+    //} else if (argc == 2) {
+    //    runFile(argv[1]);
+    //} else {
+    //    fprintf(stderr, "Usage: clox [path]\n");
+    //    exit(64);
+    //}
 
     freeVM();
     return 0;
